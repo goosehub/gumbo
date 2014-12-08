78c4c5bf802bdf87b046214d0ab7a82f1d62b83c
Goose <goosemixes@gmail.com>
2014-12-08 18:10:30 -0500

    Functional and Styled
    Mostly finished

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,45 @@
 $(document).ready(function()
 {
+// Preloader
+	$(window).load(function(){ 
+    $('#preloader').fadeOut(2000,function(){$(this).remove();});
+});
+// Loading Animations
+	$('#logo').addClass('animated rotateIn');
+	$('#nav-wrap').addClass('animated lightSpeedIn');
+	$('#main-headline').addClass('animated zoomIn');
+	$('#carousel-example-generic').addClass('animated fadeInUp');
+	$('#let-the-good-times-roll').addClass('animated zoomInDown');
+	$('#main-info').addClass('animated zoomInLeft');
+	$('#links').addClass('animated zoomInUp');
+// Carousel interval
+$('.carousel').carousel({
+  interval: 6000
+})
+
+// Our Story
+	$('#story-headline').addClass('animated zoomIn');
+	$('#story-1').addClass('animated bounceInLeft');
+	$('#story-2').addClass('animated bounceInRight');
+
+// Menus
+	$('#menu-cnt').addClass('animated flipInX');
+
+// Testimonials
+	$('#testimonial-headline').addClass('animated flipInX');
+	$('#testimonials-cnt').addClass('animated fadeInUpBig');
+
+// Community
+	$('#community-headline').addClass('animated flipInX');
+	$('.community-item').addClass('animated fadeInUpBig');
+
+// Jokes
+	$('#joke-headline').addClass('animated flipInX');
+	$('#joke-sub-headline').addClass('animated flipInY');
+	$('.joke-item').addClass('animated fadeInUpBig');
+
+// Club
+	$('#club-headline').addClass('animated flipInX');
+	$('#form-cnt').addClass('animated fadeInUpBig');
 
 }); //End Document
